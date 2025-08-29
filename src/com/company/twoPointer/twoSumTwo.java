5bfb69632699e249ce38ccc8f0f325bd356375f5
Ahnaf Faisal <ahnaffaisalalavi@gmail.com>
2025-08-29 13:18:55 +0600

    two sum two

---
+++ src/com/company/twoPointer/twoSumTwo.java
@@ -0,0 +1,21 @@
+package com.company.twoPointer;
+
+public class twoSumTwo {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        int[] result = new int[2];
+        while (left < right){
+            int sum = numbers[left] + numbers[right];
+            if (sum == target){
+                result[0] = left + 1;
+                result[1] = right + 1;
+                break;
+            } else if (sum < target){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
