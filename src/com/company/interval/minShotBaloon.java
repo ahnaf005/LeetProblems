537a42772554d12d58a7bf6e0bb6798eb8b87b2c
Ahnaf Faisal <ahnaffaisalalavi@gmail.com>
2025-08-31 17:10:32 +0600

    baloon

---
+++ src/com/company/interval/minShotBaloon.java
@@ -0,0 +1,23 @@
+package com.company.interval;
+
+import java.util.Arrays;
+
+public class minShotBaloon {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int arrows = 1;
+        int xStart, xEnd, firstEnd = points[0][1];
+        for (int[] p : points) {
+            xStart = p[0];
+            xEnd = p[1];
+
+            // If the current balloon starts after the end of another one,
+            // one needs one more arrow
+            if (firstEnd < xStart) {
+                arrows++;
+                firstEnd = xEnd;
+            }
+        }
+        return arrows;
+    }
+}
