52f351bd9887e457812cdd0684c2a658c3991dbd
Ahnaf Faisal <38178127+ahnaf005@users.noreply.github.com>
2026-07-18 15:29:18 -0700

    problems

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 	// write your code here
+        String a = "hello";
+        String b = "hello";
+        System.out.println(a == b);
         System.out.println("repo created");
     }
 }
